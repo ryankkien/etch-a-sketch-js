fdba88d1bf88a979ff1e66c4e8e020ce96cb534e
ryankkien <ryankien@ucla.edu>
2023-07-03 18:35:51 -0700

    fixed filename referneces in html

--- myscript.js
+++ myscript.js
@@ -1,11 +1,13 @@
 let square = document.createElement("div");
-square.style.width = "100px";
-square.style.height = "100px";
+square.style.width = "20px";
+square.style.height = "20px";
 square.style.backgroundColor = "white";
 square.style.border = "1px solid black";
 
 for(let i = 0; i < 16; i++){
     for(let j = 0; j < 16; j++){
-        document.container.appendChild(square)
-    }
-}
+        $("#container").append("<div class='grid'></div>");
+    };
+};
+$(".grid").width(960/x);
+$(".grid").height(960/x);
